27830db1b14b2b0ff09be736b7cae3e05fe72e12
Javzero <javzero@hotmail.com>
2018-11-21 01:06:31 +0000

    Changes from server

--- app/Http/Config.php
+++ app/Http/Config.php
@@ -1,6 +1,6 @@
 <?php 
     
-    define('APP_EMAIL_1','info@brunaindumentaria.com.ar');    
-    //define('APP_EMAIL_1','dev@vimana.studio');
+    //define('APP_EMAIL_1','info@augustamoi.com.ar');    
+    define('APP_EMAIL_1','dev@vimana.studio');
     define('APP_EMAIL_2','dev@vimana.studio');
     define('APP_BUSSINESS_NAME', 'Augustamoi');
