34de58db2564ff9892fdfef04539b5c1c8108213
javzero <javzero1@gmail.com>
2019-08-05 21:31:27 -0300

    New home banner

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -3,20 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\CatalogArticle;
 use App\Customer;
-use App\Contact;
 use App\Cart;
 use Carbon\Carbon;
-//use Mail;
-//use App\Mail\SendMail;
-//use App\Settings;
 
 //1)Ventas en $ y en unidades de todas las marcas x mes. ✓
 //2)Cantidad de pedidos cerrados mes a mes. ✓
 //3)Cantidad de clientes distintos que compran x mes. 
 //4)Cantidad de registros x mes ✓
-//5) saber cuál es el día de la semana que más pedidos se hacen y el que menos se hacen.
+//5)saber cuál es el día de la semana que más pedidos se hacen y el que menos se hacen.
 
 class StatsController extends Controller
 {   
@@ -159,13 +154,6 @@ class StatsController extends Controller
         return array(['data' => $data, 'exec_time' => $execTime]);
     }
 
-    
-    public function getChartData($period)
-    {
-        $result = $this->salesByPeriod($period);
-        return response()->json($result);
-    }
-
     public function statsCheck($brand, $period)
     {
         // $carts = Cart::where('created_at', '>', (new \Carbon\Carbon)->submonths($period))->where('status', 'Finished')->get();
@@ -208,33 +196,4 @@ class StatsController extends Controller
         return $data;
     }
 
-    public function getSellsPerMonth($month)
-    {
-        // Sales Per Month
-        $carts = Cart::where('status', 'Finished')->whereRaw('MONTH(created_at) = ?',[$month])->get();
-        // Total Sales
-
-        // dd($carts);
-        
-        
-        
-        // $value = Cart::where('status', 'Finished')
-        //         ->whereBetween('created_at', [$init_date, $last_date])
-        //         ->get;
-
-        // $value;
-
-        // $oneMonthData = $collection->where('created_at', '>', Carbon::parse($now->month)->startOfMonth())->where('created_at', '<', Carbon::parse($someDateString)->endOfMonth());
-
-        $totalbuys = 0;
-        foreach ($carts as $cart) {
-            foreach($cart->items as $item)
-            $price = ($item->final_price * $item->quantity);
-            // dd('F price '. $item->final_price. ' x Quant' .  $item->quantity . ' = ' . $price);
-            
-            $totalbuys += $price;
-        }
-        // dd($totalbuys);
-
-    }
 }
