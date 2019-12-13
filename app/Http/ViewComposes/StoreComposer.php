16d228d4df45a3a299b90f5592f5df7c11014d6b
javzero <javzero1@gmail.com>
2019-12-12 23:06:30 -0300

    New Store UI

--- app/Http/ViewComposes/StoreComposer.php
+++ app/Http/ViewComposes/StoreComposer.php
@@ -5,6 +5,7 @@ namespace App\Http\ViewComposers;
 use Illuminate\Contracts\View\View;
 use App\Traits\CartTrait;
 use App\Settings;
+use App\CatalogBrand;
 
 class StoreComposer
 {
@@ -16,10 +17,12 @@ class StoreComposer
         $google_analytics = $settings->google_analytics;
         $favs = $this->getCustomerFavs();
         $activeCart = $this->activeCart();
+        $brands = CatalogBrand::orderBy('name', 'asc')->get();
         $view
             ->with('activeCart', $activeCart)
             ->with('favs', $favs)
             ->with('settings', $settings)
-            ->with('google_analytics', $google_analytics);
+            ->with('google_analytics', $google_analytics)
+			->with('brands', $brands);;
 	}
 }
--- resources/views/store/partials/categories.blade.php
+++ resources/views/store/partials/categories.blade.php
@@ -2,9 +2,9 @@
 	<div class="container">
 		<div class="row">
 			<ul>
-				<li><a href="{{ route('store', 'categoria=para_arriba') }}">Para arriba</a></li>
-				<li><a href="{{ route('store', 'categoria=para_abajo') }}">Para abajo</a></li>
-				<li><a href="{{ route('store', 'categoria=vestidos') }}">Vestidos</a></li>
+				<li><a href="{{ route('store', 'categoria=58') }}">Para arriba</a></li>
+				<li><a href="{{ route('store', 'categoria=59') }}">Para abajo</a></li>
+				<li><a href="{{ route('store', 'categoria=32') }}">Vestidos</a></li>
 			</ul>		
 		</div>
 	</div>
