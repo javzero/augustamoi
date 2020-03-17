a751950b8fac5b94c7390dc2a176a1875c17be0f
javzero <javzero1@gmail.com>
2020-03-17 01:20:11 -0300

    New featured articles

--- resources/views/vadmin/partials/adminNav.blade.php
+++ resources/views/vadmin/partials/adminNav.blade.php
@@ -175,6 +175,13 @@
 					<a href="{{ route('catalogo.index', ['status' => '0']) }}" class="menu-item">
 					<i class="icon-list"></i> Pausados</a>
 				</li>
+				{{-- Article List Featured--}}
+				<li class="@if(app('request')->input('redirect') != 'stock')
+					{{ Menu::activeMenu('catalogo') }}
+					@endif">
+					<a href="{{ route('catalogo.index', ['featured' => '1']) }}" class="menu-item">
+					<i class="icon-star"></i> Destacados</a>
+				</li>
 				{{-- Article Stock --}}
 				{{-- <li class="@if(app('request')->input('redirect') == 'stock') active @endif">
 					<a href="{{ route('catalogo.index', ['redirect' => 'stock']) }}" class="menu-item">
